44ad09fe7ddf86e2ab0a65bc7ebde62c4cce6efa
richy0911 <seaford.bacchas@mymona.uwi.edu>
2017-02-26 09:31:55 +0000

    exercise 8

--- app/views.py
+++ app/views.py
@@ -28,35 +28,47 @@ def about():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        # if user is already logged in, just redirec them to our secure page
+        # or some other page like a dashboard
+        return redirect(url_for('secure_page'))
     form = LoginForm()
     if request.method == "POST":
         # change this to actually validate the entire form submission
         # and not just one field
         if form.username.data:
             # Get the username and password values from the form.
+            username=form.username.data
+            password=form.password.data
 
-            # using your model, query database for a user based on the username
-            # and password submitted
             # store the result of that query to a `user` variable so it can be
             # passed to the login_user() method.
-
+            user=UserProfile.query.filter_by(username=username, password=password).first()
+            
+            if user is not None:
             # get user id, load into session
-            login_user(user)
-
-            # remember to flash a message to the user
-            return redirect(url_for("home")) # they should be redirected to a secure-page route instead
+                login_user(user)
+    
+                # remember to flash a message to the user
+                flash('Looks like everything worked out fine!', 'success')
+                next = request.args.get('next')
+                return redirect(url_for('secure_page'))
+            else:
+                flash('Username or Password is incorrect.', 'danger')
+                return redirect(url_for("home")) # they should be redirected to a secure-page route instead
     return render_template("login.html", form=form)
 
 # user_loader callback. This callback is used to reload the user object from
 # the user ID stored in the session
 @login_manager.user_loader
 def load_user(id):
-    return UserProfile.query.get(int(id))
-
-###
-# The functions below should be applicable to all Flask apps.
-###
-
+    return UserProfile.query.get(int(id)
+    
+def flash_errors(form):
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash(u"Error in the %s field - %s" % (getattr(form, field).label.text,error),'danger')
+    
 @app.route('/<file_name>.txt')
 def send_text_file(file_name):
     """Send your static text file."""
